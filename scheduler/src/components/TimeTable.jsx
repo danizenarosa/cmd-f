e28c3fdeaeb95e7720ab040203dc6afc93cf3551
dani <135657902+danizenarosa@users.noreply.github.com>
2025-03-08 19:07:30 -0800

    made TimeTable dynamic

--- scheduler/src/components/TimeTable.jsx
+++ scheduler/src/components/TimeTable.jsx
@@ -1,4 +1,65 @@
+import CourseCard from "./CourseCard";
+import schedule from BACKEND;
+
+const times_strings = ["8:00 am", 
+              "8:30 am", 
+              "9:00 am", 
+              "9:30 am", 
+              "10:00 am", 
+              "10:30 am", 
+              "11:00 am", 
+              "11:30 am", 
+              "12:00 pm", 
+              "12:30 pm", 
+              "1:00 pm", 
+              "1:30 pm", 
+              "2:00 pm", 
+              "2:30 pm", 
+              "3:00 pm", 
+              "3:30 pm", 
+              "4:00 pm", 
+              "4:30 pm", 
+              "5:00 pm", 
+              "5:30 pm", 
+              "6:00 pm", 
+              "6:30 pm", 
+              "7:00 pm", 
+              "7:30 pm", 
+              "8:00 pm", 
+              "8:30 pm", 
+              "9:00 pm", 
+              "9:30 pm"]
+
+const makeDaySched = () => {
+  let schedules = schedule.days;
+  let code;
+  let section;
+  let location;
+  return (
+    schedules.map((day, courses) => {
+      return (
+    <table>
+    <thead>
+      <tr>
+        <th>{day}</th>
+      </tr>
+    </thead>
+    <tbody>
+      {courses.map((c) => {
+        code = c.code;
+        section = c.section;
+        location = c.location;
+        CourseCard(code, section, location);
+      })}
+    </tbody>
+  </table>
+  )})
+  );
+}
+
 const TimeTable = () => {
+  const day_schedules = schedule.schedules;
+  const times = times_strings.slice(schedule.earliest, schedule.latest + 1);
   return (
     <span>
       <table>
@@ -8,118 +69,14 @@ const TimeTable = () => {
           </tr>
         </thead>
         <tbody>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
+          {times.map((t) => {
+            <tr>
+              <td>t</td>
+            </tr>
+          })}
         </tbody>
       </table>
-
-      <table>
-        <thead>
-          <tr>
-            <th>Monday</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-        </tbody>
-      </table>
-
-      <table>
-        <thead>
-          <tr>
-            <th>Tuesday</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-        </tbody>
-      </table>
-
-      <table>
-        <thead>
-          <tr>
-            <th>Wednesday</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-        </tbody>
-      </table>
-
-      <table>
-        <thead>
-          <tr>
-            <th>Thursday</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-        </tbody>
-      </table>
-
-      <table>
-        <thead>
-          <tr>
-            <th>Friday</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
-        </tbody>
-      </table>
+      {makeDaySched()};
     </span>
   );
 };
